2705584bb7f7acfc63074ed76a0da17a5621b9dc
steve <steve.lorent@bstorm.be>
2024-01-12 09:54:55 +0100

    Ajoutez des fichiers projet.

---
+++ Cloud_ConsoAPI_Console/Program.cs
@@ -0,0 +1,57 @@
+﻿using Cloud_ConsoAPI_Console;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+string url = "https://localhost:7152/api/";
+
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri(url);
+
+
+#region Get
+
+List<Movie> liste = new List<Movie>();
+
+try
+{
+    using (HttpResponseMessage message = await client.GetAsync("movie"))
+    {
+        message.EnsureSuccessStatusCode();
+
+        string json = await message.Content.ReadAsStringAsync();
+        Console.WriteLine(json);
+
+        liste = JsonConvert.DeserializeObject<List<Movie>>(json);
+
+    }
+}
+catch (HttpRequestException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+foreach (Movie movie in liste)
+{
+    Console.WriteLine($"{movie.Id} - {movie.Title} - {movie.ReleaseYear}");
+}
+
+#endregion
+
+
+#region POST
+
+Movie m = new Movie() { Id = 5, Title = "Test Conso API", ReleaseYear = 2024 };
+
+string newJson = JsonConvert.SerializeObject(m);
+HttpContent content = new StringContent(newJson, Encoding.UTF8, "application/json");
+
+using (HttpResponseMessage message = await client.PostAsync("movie", content))
+{
+    if (message.StatusCode == HttpStatusCode.OK)
+    {
+        Console.WriteLine("ok");
+    }
+} 
+#endregion
